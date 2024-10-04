62fc5ea0a5aa1c9d96124d4f5c5314789691aec6
Utkarsh vyas <122040219+utkarshaaaaa@users.noreply.github.com>
2024-10-04 13:04:22 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+type URL struct {
+	ID           string    `json:"id"`
+	OriginalURL  string    `json:"original_url"`
+	ShortURL     string    `json:"short_url"`
+	CreationDate time.Time `json:"creation_date"`
+}
+
+var urlDB = make(map[string]URL)
+
+func generateShortURL(OriginalURL string) string {
+	hasher := md5.New()
+	hasher.Write([]byte(OriginalURL))
+	data := hasher.Sum(nil)
+	hash := hex.EncodeToString(data)
+
+	fmt.Println("final string: ", hash[:8])
+	return hash[:8]
+}
+
+func createURL(originalURL string) string {
+	shortURL := generateShortURL(originalURL)
+	id := shortURL
+	urlDB[id] = URL{
+		ID:           id,
+		OriginalURL:  originalURL,
+		ShortURL:     shortURL,
+		CreationDate: time.Now(),
+	}
+	return shortURL
+}
+
+func getURL(id string) (URL, error) {
+	url, ok := urlDB[id]
+	if !ok {
+		return URL{}, errors.New("URL not found")
+	}
+	return url, nil
+}
+
+func ShortURLHandler(w http.ResponseWriter, r *http.Request) {
+	var data struct {
+		URL string `json:"url"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	shortURL_ := createURL(data.URL)
+
+	response := struct {
+		ShortURL string `json:"short_url"`
+	}{ShortURL: shortURL_}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func redirectURLHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("/redirect/"):]
+	url, err := getURL(id)
+	if err != nil {
+		http.Error(w, "Invalid request", http.StatusNotFound)
+	}
+	http.Redirect(w, r, url.OriginalURL, http.StatusFound)
+}
+
+func main() {
+	router := mux.NewRouter()
+
+	router.HandleFunc("/shorten", ShortURLHandler).Methods("POST")
+	router.HandleFunc("/redirect/", redirectURLHandler).Methods("GET")
+
+	fmt.Println("Starting server on port 3001...")
+	err := http.ListenAndServe(":3001", handlers.CORS()(router))
+	if err != nil {
+		fmt.Println("Error on starting server:", err)
+	}
+}
